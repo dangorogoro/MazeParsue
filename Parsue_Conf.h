dc1d5fd4fe029900f890ef2cbce276decaed2758
dangorogoro <dangorogoro0802@gmail.com>
2019-08-23 13:30:04 +0900

    Change macro name

--- Parsue_Conf.h
+++ Parsue_Conf.h
@@ -1,5 +1,5 @@
-#ifndef MAZESOLVER_CONF_H_
-#define MAZESOLVER_CONF_H_
+#ifndef PARSUE_CONF_H_
+#define PARSUE_CONF_H_
 constexpr uint32_t MAZE_SIZE = 32;
 constexpr float MAZE_1BLOCK_LENGTH = 0.09;
 
